8946d37132b8d8aa764575cf1a8c122b4312685e
KrasnovDenis <elo.1972@yandex.ru>
2021-01-19 02:36:10 +0300

    modified : Vehicles layout

--- src/components/customer/VehicleLabel.js
+++ src/components/customer/VehicleLabel.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import "../../style/components/customer/Vehicle.css";
+import carPic from '../../style/images/icons/car_pic.jpeg'
 
 //значок машины и краткое описание
 export function VehicleLabel(props) {
     return (
         <div className="car jumbotron">
-            <img  className="car-img-top" alt="машина"/>
+            <img  className="car-img-top" alt="машина" width="150px" style={{borderRadius: "50px"}} src={carPic}/>
                 <div className="car-body">
                     <h5 className="car-model">{props.cars.model}</h5>
                     <p className="car-description">{props.cars.description}</p>
                     <Link to={ '../vehicle/' + props.cars.id}  className="btn btn-primary">Перейти к машине</Link>
                 </div>
         </div>
+
+
     );
 
 }
--- src/pages/user/Vehicles.js
+++ src/pages/user/Vehicles.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {SideBar} from "../../components/SideBar";
 import {setUpSideBar} from "./PrivateArea";
 import {VehicleLabel} from "../../components/customer/VehicleLabel";
+import {Col, Container, Row} from "reactstrap";
 
 class Vehicles extends Component {
     render() {
@@ -9,9 +10,32 @@ class Vehicles extends Component {
         return (
             <div className="feed">
                 <SideBar menuItems={sideBarItems}/>
-                <div className='content-customer'>
-                    <VehicleLabel cars={getCarInfoStub()}/>
-                </div>
+                <Container className='content-customer' style={{ maxWidth: "990px"}} fluid="true">
+                    <Row>
+                        <Col>
+                            <h3 style={{ textDecoration: "underline"}}>
+                                <i>Все автомобили</i>
+                            </h3>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col className="sm-1 lg-1">
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                            <VehicleLabel cars={getCarInfoStub()}/>
+                        </Col>
+                    </Row>
+
+                </Container>
             </div>
         )
     }
@@ -22,7 +46,8 @@ export default Vehicles
 function getCarInfoStub() {
     return {
         id: "1",
-        owner: "ООО Зеленоглазое Такси",
-        name: "Парк сокольники"
+        model: "Ford Mustang",
+        park: "Парк сокольники",
+        description: "}{орошая машина"
     };
 }
