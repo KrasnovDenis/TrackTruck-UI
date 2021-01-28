c61c7c715c29b1190bfcdadb771829eb13cef1e9
KrasnovDenis <elo.1972@yandex.ru>
2021-01-29 01:46:05 +0300

    added feedback static layout

--- src/user/Feedback.js
+++ src/user/Feedback.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {SideBar} from "../common/SideBar";
 import {setUpSideBar} from "./PrivateArea";
+import {Col, Container, Row} from "reactstrap";
+import '../style/components/customer/Feedback.css';
+import supportAvatar from '../style/images/icons/support-avatar.png';
+import companyAvatar from '../style/images/icons/company-logo.png';
 
 class Feedback extends Component {
     render() {
@@ -11,10 +15,35 @@ class Feedback extends Component {
             <div className="feed">
                 <SideBar menuItems={sideBarItems}/>
                 <div className="content-customer">
-                    Связаться с администрацией
-                    <form>
+                    <h2>Связаться с администрацией</h2>
+                   <Container>
+                       <Row>
+                           <Col>
+                               <div className="container">
+                                   <img src={companyAvatar}  className="avatar right" style={{width:"60px"}} alt="Avatar" />
+                                       <p>Hello. How are you today?</p>
+                                       <span className="time-right">11:00</span>
+                               </div>
 
-                    </form>
+                               <div className="container darker">
+                                   <img src={supportAvatar} alt="Avatar" style={{width:"60px"}} className="avatar"/>
+                                       <p>Hey! I'm fine. Thanks for asking!</p>
+                                       <span className="time-left">11:01</span>
+                               </div>
+                               <div className="container">
+                                   <img src={companyAvatar}  className="avatar right" style={{width:"60px"}} alt="Avatar" />
+                                       <p>Sweet! So, what do you wanna do today?</p>
+                                       <span className="time-right">11:02</span>
+                               </div>
+
+                               <div className="container darker" >
+                                   <img src={supportAvatar}  className="avatar " style={{width:"60px"}} alt="Avatar" />
+                                       <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                                       <span className="time-left">11:05</span>
+                               </div>
+                           </Col>
+                       </Row>
+                   </Container>
                 </div>
             </div>
         )
