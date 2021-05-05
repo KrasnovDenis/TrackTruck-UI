4971fa958e4bd99fa26dacd4273d180ce4ca0dc7
KrasnovDenis <Elo.1972@yandex.ru>
2021-05-06 01:05:09 +0300

    added: some small layout

--- src/common/Home.js
+++ src/common/Home.js
@@ -8,11 +8,10 @@ class Home extends Component {
 
     render() {
         return (
-            <div>
-                <Container fluid="true">
+            <div className="homeContainer">
+                <Container style={{marginLeft: "6%", backgroundColor: "#fff"}}>
                     <Row>
-                        <Col className="sideCol" sm="1"/>
-                        <Col style={{fontFamily: "Impact", fontSize: "16px"}}>
+                        <Col md="5" style={{fontFamily: "Impact", fontSize: "16px"}}>
                             <h1>Автопарк расширяется ? </h1>
                             <h1>Мониторинг отнимает много времени?</h1>
                             <hr/>
@@ -29,22 +28,18 @@ class Home extends Component {
                                 <li key="point3">Понятные графики</li>
                             </ul>
                         </Col>
-                        <Col>
-                            <img src={sweater} alt="Кортиночка" width="450px"/>
+                        <Col md="4">
+                            <img src={sweater} alt="Кортиночка" width="500px"/>
                         </Col>
-                        <Col className="sideCol" sm="1"/>
                     </Row>
                     <Row className="Benefit">
                         <Col>
                             <h3 style={{textAlign: "center"}}> Наши основные преимущества</h3>
+                        {/*    todo: make explanation why monitoring is important*/}
                         </Col>
                         <br/>
-                    </Row>
-                    <Row className="Benefit">
-                        <hr/>
-                        <Col sm="2"/>
-
-                        <Col sm="2"/>
+                        <br/>
+                        <br/>
                     </Row>
                 </Container>
 
